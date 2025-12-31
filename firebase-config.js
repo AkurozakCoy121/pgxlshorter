4f7a8bb0e4441c9cdacaf1962d06b3d2dcd7bc94
AkurozakCoy121 <ojakgamteng62@gmail.com>
2025-12-31 19:30:37 +0700

    Update and rename firebase-config.json to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// FILE INI YANG SIMPAN FIREBASE CONFIG!
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCH5nmdOf3tL7HNjx9kqBmISgaylG8vvOU",
+    authDomain: "pgxl-4567a.firebaseapp.com",
+    projectId: "pgxl-4567a",
+    storageBucket: "pgxl-4567a.firebasestorage.app",
+    messagingSenderId: "545920930826",
+    appId: "1:545920930826:web:18458fd3382a83f17abbf3",
+    measurementId: "G-1Y4E635M4R"
+};
